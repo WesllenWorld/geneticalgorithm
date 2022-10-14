bbb652340d8563c0cfb35c5538feaf7c56d7af9f
Wesllen Leonardo de Lira Santos <wesllen.2020111052@unicap.br>
2022-10-14 12:48:46 +0000

    implentação básica do individuo

--- src/program/Individup.java
+++ src/program/Individup.java
@@ -2,7 +2,26 @@ package program;
 
 public class Individup {
 
+    private int x1, x2;
 
+    public Individup(int x1, int x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
 
+    public int getX1() {
+        return x1;
+    }
 
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
 }
